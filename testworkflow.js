a1239cf631fbc0f0409764e6db2ecb6795851aab
d2lam <daolam112@gmail.com>
2019-10-02 17:29:13 -0700

    add pipelineId

---
+++ testworkflow.js
@@ -0,0 +1,8 @@
+// const getNextJobs = require('./lib/getNextJobs');
+// const getWorkflow = require('./lib/getWorkflow');
+// const getSrcForJoin = require('./lib/getSrcForJoin');
+// const REQUIRES_WORKFLOW = require('./test/data/requires-workflow-exttrigger');
+//
+// const wf = getWorkflow(REQUIRES_WORKFLOW);
+// // const nextJob = getNextJobs(wf, {trigger:'~sd1234:foo'}))
+// console.log(getSrcForJoin(wf, {jobName: 'sd@4321:bar'}));
