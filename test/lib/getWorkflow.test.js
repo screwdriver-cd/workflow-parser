06b0e350cd5ffc9e87e3fc248e5b730ab2288af6
d2lam <daolam112@gmail.com>
2019-10-03 17:40:08 -0700

    recursively dfs on remote edges

--- test/lib/getWorkflow.test.js
+++ test/lib/getWorkflow.test.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const assert = require('chai').assert;
+const sinon = require('sinon');
 const getWorkflow = require('../../lib/getWorkflow');
-
 const REQUIRES_WORKFLOW = require('../data/requires-workflow');
 const LEGACY_AND_REQUIRES_WORKFLOW = Object.assign({}, REQUIRES_WORKFLOW);
-
 const EXTERNAL_TRIGGER = require('../data/requires-workflow-exttrigger');
-
 const EXPECTED_OUTPUT = require('../data/expected-output');
 const NO_EDGES = Object.assign({}, EXPECTED_OUTPUT);
 const EXPECTED_EXTERNAL = require('../data/expected-external');
@@ -15,6 +13,10 @@ const EXPECTED_EXTERNAL = require('../data/expected-external');
 NO_EDGES.edges = [];
 
 describe('getWorkflow', () => {
+    const triggerFactoryMock = {
+        getDestFromSrc: sinon.stub().resolves([])
+    };
+
     it('should throw if it is not given correct input', async () => {
         try {
             await getWorkflow({ config: {} });
@@ -164,6 +166,61 @@ describe('getWorkflow', () => {
                 { src: 'foo', dest: 'baz' },
                 { src: 'bar', dest: 'bax', join: true },
                 { src: 'baz', dest: 'bax', join: true }
+            ]
+        });
+    });
+
+    it('should handle external upstream & downstream join', async () => {
+        /* A -> B                                                   --> C
+                sd@111:external-level1 -> sd@222:external-level2
+                sd@333:external-level1 -> sd@444:external-level2
+        */
+
+        triggerFactoryMock.getDestFromSrc.withArgs('sd@123:A').resolves([
+            'sd@111:external-level1',
+            'sd@333:external-level1'
+        ]);
+        triggerFactoryMock.getDestFromSrc.withArgs('sd@111:external-level1').resolves([
+            'sd@222:external-level2'
+        ]);
+        triggerFactoryMock.getDestFromSrc.withArgs('sd@333:external-level1').resolves([
+            'sd@444:external-level2'
+        ]);
+
+        const result = await getWorkflow({
+            jobs: {
+                A: { },
+                B: { requires: ['A'] },
+                C: { requires: [
+                    'B',
+                    'sd@222:external-level2',
+                    'sd@444:external-level2'
+                ] }
+            }
+        }, triggerFactoryMock, 123);
+
+        console.log(result);
+        assert.deepEqual(result, {
+            nodes: [
+                { name: '~pr' },
+                { name: '~commit' },
+                { name: 'A' },
+                { name: 'B' },
+                { name: 'C' },
+                { name: 'sd@111:external-level1' },
+                { name: 'sd@222:external-level2' },
+                { name: 'sd@333:external-level1' },
+                { name: 'sd@444:external-level2' }
+            ],
+            edges: [
+                { src: 'A', dest: 'B' },
+                { src: 'B', dest: 'C', join: true },
+                { src: 'sd@222:external-level2', dest: 'C', join: true },
+                { src: 'sd@444:external-level2', dest: 'C', join: true },
+                { src: 'A', dest: 'sd@111:external-level1' },
+                { src: 'A', dest: 'sd@333:external-level1' },
+                { src: 'sd@111:external-level1', dest: 'sd@222:external-level2' },
+                { src: 'sd@333:external-level1', dest: 'sd@444:external-level2' }
             ]
         });
     });
